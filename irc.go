3103db19a993b83da99dceb078d20931514cb9cd
awoitte <aliciawoitte@gmail.com>
2018-02-26 17:40:30 -0500

    adds channel management

--- irc.go
+++ irc.go
@@ -1,25 +1,46 @@
 package irc
 
+import "errors"
+
 type IRC struct {
 	connection Chat_stream
+	channel    string
 }
 
 func Connect(name string, conn Chat_stream) IRC {
-	irc := IRC{conn}
+	irc := IRC{conn, ""}
 	irc.SendRaw("NICK " + name)
 	irc.SendRaw("USER " + name + " * 0 :" + name + " " + name)
 	return irc
 }
 
-func (irc IRC) SendMessage(channel, message string) {
-	irc.SendRaw("PRIVMSG " + channel + " " + message)
+func (irc *IRC) SendMessage(message string) error {
+	if irc.channel == "" {
+		return errors.New("You must join a channel before you can send a message")
+	}
+	irc.SendRaw("PRIVMSG #" + irc.channel + " " + message)
+	return nil
 }
 
-func (irc IRC) SendRaw(message string) {
+func (irc *IRC) SendRaw(message string) {
 	irc.connection.Write(message + "\r\n")
 }
 
-func (irc IRC) ReadLoop(chunk_size int, respond func(string) error) error {
+func (irc *IRC) JoinChannel(channel string) {
+	irc.channel = channel
+	irc.SendRaw("JOIN #" + channel)
+}
+
+func (irc *IRC) PartChannel() error {
+	if irc.channel == "" {
+		return errors.New("You must join a channel before you can send a message")
+	}
+	irc.SendRaw("PART #" + irc.channel)
+	irc.channel = ""
+	return nil
+}
+
+func (irc *IRC) ReadLoop(chunk_size int, respond func(string) error) error {
 	for {
 		data, err := irc.connection.Read()
 		if err != nil {
--- irc_test.go
+++ irc_test.go
@@ -14,15 +14,6 @@ func Test_it_should_send_nick_and_user_on_connect(t *testing.T) {
 
 }
 
-func Test_it_should_add_channel_and_privmsg(t *testing.T) {
-	stream := MockStream{}
-	irc := Connect("test", &stream)
-
-	stream.StartListening()
-	go irc.SendMessage("chan", "test")
-	expect_write(t, stream.writes, "PRIVMSG chan test\r\n")
-}
-
 func Test_it_should_attach_line_breaks(t *testing.T) {
 	stream := MockStream{}
 	irc := Connect("test", &stream)
@@ -46,5 +37,52 @@ func Test_it_should_read_until_Err_return(t *testing.T) {
 
 	if calls != 2 {
 		t.Errorf("called too many times %d", calls)
+	}
+}
+
+func Test_it_should_send_join_message_on_join(t *testing.T) {
+	stream := &MockStream{}
+	irc := Connect("test", stream)
+
+	stream.StartListening()
+	go irc.JoinChannel("test")
+	expect_write(t, stream.writes, "JOIN #test\r\n")
+}
+
+func Test_it_should_add_channel_and_privmsg(t *testing.T) {
+	stream := MockStream{}
+	irc := Connect("test", &stream)
+	irc.JoinChannel("chan")
+
+	stream.StartListening()
+	go irc.SendMessage("test")
+	expect_write(t, stream.writes, "PRIVMSG #chan test\r\n")
+}
+
+func Test_it_should_require_channel_to_send_message(t *testing.T) {
+	stream := &MockStream{}
+	irc := Connect("test", stream)
+	err := irc.SendMessage("test_message")
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func Test_it_should_send_part_message(t *testing.T) {
+	stream := MockStream{}
+	irc := Connect("test", &stream)
+	irc.JoinChannel("chan")
+
+	stream.StartListening()
+	go irc.PartChannel()
+	expect_write(t, stream.writes, "PART #chan\r\n")
+}
+
+func Test_it_should_require_channel_to_send_part(t *testing.T) {
+	stream := &MockStream{}
+	irc := Connect("test", stream)
+	err := irc.PartChannel()
+	if err == nil {
+		t.Fail()
 	}
 }
